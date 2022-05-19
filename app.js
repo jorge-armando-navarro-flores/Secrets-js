a1514e881cd5b90b13ecc6b34c16dd07f60673d0
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2022-05-19 13:54:46 -0500

    submit

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -92,11 +93,41 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
+  User.find({"secret": {$ne: null}}, (err, foundUsers) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login")
   }
+});
+
+app.post("/submit", (req, res) => {
+  const submittedSecret = req.body.secret;
+
+  console.log(req.user);
+
+  User.findById(req.user.id, (err, foundUser) => {
+    if (err){
+      console.log(err);
+    } else {
+      foundUser.secret = submittedSecret;
+      foundUser.save(() => {
+        res.redirect("/secrets");
+      });
+    }
+  });
+
 });
 
 app.get("/logout", (req, res) => {
